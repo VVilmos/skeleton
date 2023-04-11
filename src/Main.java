133a9bd3ad5e3239af6ed8e2894998f0cf733990
Vilmos Vörös <vvoros@edu.bme.hu>
2023-04-11 15:30:55 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import Model.Cistern;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        Cistern cis = new Cistern();
+        System.out.println(cis.toString());
+    }
+}
---
+++ src/Skeleton.java
@@ -0,0 +1,7 @@
+public class Skeleton {
+    private int indent = 0;
+
+    public void Start() {}
+
+    public void End() {}
+}
