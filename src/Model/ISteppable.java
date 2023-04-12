bdb29d65c2d5f36dfd26e8869b0bcc58181ca323
Vilmos Vörös <vvoros@edu.bme.hu>
2023-04-12 23:32:41 +0200

    Model classes edited

---
+++ src/Model/ISteppable.java
@@ -0,0 +1,6 @@
+package Model;
+
+public interface ISteppable {
+
+    void step();
+}
---
+++ src/Tester.java
@@ -0,0 +1,25 @@
+public class Tester {
+
+    public void menu() {
+        while(true) {
+            //println  1: Repairing leaking Pipe
+            //2: ...
+
+            //sorszám bekérés
+
+           // if (1) repairs_leaking_pipe();
+        }
+
+
+    }
+
+    /*
+    public void repairs_leaking_pipe() {
+        //init a komm diagram alapján
+
+        //repairPipe, acceptwater, step
+
+    }*/
+
+
+}
